0ffdabce3a38f615968bc2302d8162358e88145f
worthamjh <worthamjh@gmail.com>
2026-02-07 14:12:06 -0600

    Add fake async helpers and demo
    Link the AsyncJava.css stylesheet in HTML and revamp the JS to demonstrate async patterns. Introduce fakeRequestCallBack (url, success, failure) and fakeRequestPromise(url) with random delays and timeout rejection, add a nested callback demo for chained requests, and comment out the earlier color-change examples and callback pyramid code for clarity.

--- Asynch Java/AsyncJava.js
+++ Asynch Java/AsyncJava.js
@@ -4,29 +4,123 @@
 // }, 3000);
 // console.log('I am at the end of the file!');
 
-setTimeout(() => {
-    document.body.style.backgroundColor = 'red';
-},3000)
-setTimeout(() => {
-    document.body.style.backgroundColor = 'orange';
-},3000)
+// setTimeout(() => {
+//     document.body.style.backgroundColor = 'red';
+//         setTimeout(() => {
+//         document.body.style.backgroundColor = 'orange';
+//             setTimeout(() => {
+//             document.body.style.backgroundColor = 'yellow';
+//                 setTimeout(() => {
+//                 document.body.style.backgroundColor = 'green';
+//                     setTimeout(() => {
+//                     document.body.style.backgroundColor = 'blue';
+//                         setTimeout(() => {
+//                         document.body.style.backgroundColor = 'indigo';
+//                             setTimeout(() => {
+//                             document.body.style.backgroundColor = 'violet';
+//                         },1000)
+//                     },1000)
+//                 },1000)
+//             },1000)
+//         },1000)
+//     },1000)
+// },1000);
 
-setTimeout(() => {
-    document.body.style.backgroundColor = 'yellow';
-},3000)
+// const colorChange = function(newColor,delay,doNext){
+//     setTimeout(() =>{
+//         document.body.style.backgroundColor = newColor;
+//         doNext();
+//     }, delay);
+// }
 
-setTimeout(() => {
-    document.body.style.backgroundColor = 'green';
-},3000)
+// colorChange('red', 3000, function(){
+//     colorChange('orange',3000,function(){
+//         colorChange('yellow',3000,function(){
+//             colorChange('green',3000,function(){
+//                 colorChange('blue',3000,function(){
+//                     colorChange('indigo',3000,function(){
+//                         colorChange('violet',3000,function(){
+//                         })
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// });
 
-setTimeout(() => {
-    document.body.style.backgroundColor = 'blue';
-},3000)
 
-setTimeout(() => {
-    document.body.style.backgroundColor = 'indigo';
-},3000)
 
-setTimeout(() => {
-    document.body.style.backgroundColor = 'violet';
-},3000)
+// *****************************************************************************
+// ASYNC PROMISES
+const fakeRequestCallBack = (url, sucess, failure) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+        if(delay > 4000) {
+            failure('Connection Timeout')
+        } else {
+            sucess(`Here is your fake data from ${url}`)
+        }
+    }, delay)
+}
+const fakeRequestPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * 4500) + 500;
+        setTimeout(() => {
+            if(delay > 4000) {
+                reject('Connection Timeout')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    }) 
+}
+
+
+
+
+
+
+
+// fakeRequestCallBack('books.com', function(){
+//     console.log('It Worked!')
+// }, function(){
+//     console.log('Error!!!!')
+// })
+
+// fakeRequestCallBack('books.com', 
+//     function(response){
+//     console.log('It Worked!')
+//     console.log(response)
+// }, function(err){
+//     console.log('Error!!!!')
+//     console.log(err)
+// })
+// ********************************************************************
+// ASYNC NESTED CALLBACKS
+fakeRequestCallBack('books.com/page1', 
+    function(response){
+        console.log('It Worked!')
+        console.log(response)
+        fakeRequestCallBack('books.com/page2',
+            function(response){
+                console.log('It Worked Again!')
+                console.log(response)
+                fakeRequestCallBack('books.com/page3',
+                    function(response){
+                        console.log('It Worked Thrice!')
+                        console.log(response)
+                    },
+                     function(err){
+                        console.log('Error Page 3!!!!')
+                        console.log(err)
+                    })
+            },
+            function(err){
+                console.log('Error Page 2!!!!')
+                console.log(err)
+            })
+    }, function(err){
+        console.log('Error!!!!')
+        console.log(err)
+})
+
