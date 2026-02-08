705b154db3f10d93541131a56de009a1e20b9a31
worthamjh <worthamjh@gmail.com>
2026-02-08 10:33:09 -0600

    Randomize fakeRequest and add usage example
    Introduce randomized success for fakeRequest (70% resolve) and resolve with a mock data string; reject on failure. Add an example invocation fakeRequest('/dogs/1') with .then and .catch handlers that log completion and errors. Note: the added .then handler logs `data` but does not accept the resolved value as an argument.

--- Asynch Java/Creating Promises/CreatingPromise.js
+++ Asynch Java/Creating Promises/CreatingPromise.js
@@ -1,8 +1,21 @@
 const fakeRequest = (url) => {
     return new Promise((resolve, reject) => {
+        const rand = Math.random();
         setTimeout(() => {
-            resolve();
+            if (rand < 0.7){
+                resolve('Your fake data here');
+            }
+            reject('Request error');
         }, 1000)
         
     })
 }
+
+fakeRequest('/dogs/1')
+    .then(() => {
+        console.log('Done with request')
+        console.log('data is:', data)
+    })
+    .catch((err) => {
+        console.log('Oh no', err)
+    })
