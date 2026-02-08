ef75536a7c7e7a7653f8318f545012e98e618de7
worthamjh <worthamjh@gmail.com>
2026-02-08 12:57:13 -0600

    Add async examples and refactor color promises
    Add new Async Functions example files and enable a nested setTimeout rainbow sequence in AsyncJava.js to demonstrate callback-based async flow. Refactor CreatingPromise.js: comment out the fakeRequest example, introduce delayedColorChange() which returns a Promise that changes document background after a delay, and chain the promises to perform sequential color transitions. These changes illustrate promise-based sequencing to replace callback nesting.

--- Asynch Java/AsyncJava.js
+++ Asynch Java/AsyncJava.js
@@ -4,27 +4,27 @@
 // }, 3000);
 // console.log('I am at the end of the file!');
 
-// setTimeout(() => {
-//     document.body.style.backgroundColor = 'red';
-//         setTimeout(() => {
-//         document.body.style.backgroundColor = 'orange';
-//             setTimeout(() => {
-//             document.body.style.backgroundColor = 'yellow';
-//                 setTimeout(() => {
-//                 document.body.style.backgroundColor = 'green';
-//                     setTimeout(() => {
-//                     document.body.style.backgroundColor = 'blue';
-//                         setTimeout(() => {
-//                         document.body.style.backgroundColor = 'indigo';
-//                             setTimeout(() => {
-//                             document.body.style.backgroundColor = 'violet';
-//                         },1000)
-//                     },1000)
-//                 },1000)
-//             },1000)
-//         },1000)
-//     },1000)
-// },1000);
+setTimeout(() => {
+    document.body.style.backgroundColor = 'red';
+        setTimeout(() => {
+        document.body.style.backgroundColor = 'orange';
+            setTimeout(() => {
+            document.body.style.backgroundColor = 'yellow';
+                setTimeout(() => {
+                document.body.style.backgroundColor = 'green';
+                    setTimeout(() => {
+                    document.body.style.backgroundColor = 'blue';
+                        setTimeout(() => {
+                        document.body.style.backgroundColor = 'indigo';
+                            setTimeout(() => {
+                            document.body.style.backgroundColor = 'violet';
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000);
 
 // const colorChange = function(newColor,delay,doNext){
 //     setTimeout(() =>{
--- Asynch Java/Creating Promises/CreatingPromise.js
+++ Asynch Java/Creating Promises/CreatingPromise.js
@@ -1,21 +1,38 @@
-const fakeRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        const rand = Math.random();
-        setTimeout(() => {
-            if (rand < 0.7){
-                resolve('Your fake data here');
-            }
-            reject('Request error');
-        }, 1000)
+// const fakeRequest = (url) => {
+//     return new Promise((resolve, reject) => {
+//         const rand = Math.random();
+//         setTimeout(() => {
+//             if (rand < 0.7){
+//                 resolve('Your fake data here');
+//             }
+//             reject('Request error');
+//         }, 1000)
         
+//     })
+// }
+
+// fakeRequest('fake.com/page1')
+//     .then((data) => {
+//         console.log('Done with request')
+//         console.log('data is:', data)
+//     })
+//     .catch((err) => {
+//         console.log('Oh no', err)
+// })
+
+const delayedColorChange = (color,delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
     })
 }
 
-fakeRequest('/dogs/1')
-    .then(() => {
-        console.log('Done with request')
-        console.log('data is:', data)
-    })
-    .catch((err) => {
-        console.log('Oh no', err)
-    })
+delayedColorChange('red', 1000)
+    .then(() => delayedColorChange('orange', 1000))
+    .then(() => delayedColorChange('yellow', 1000))
+    .then(() => delayedColorChange('green', 1000))
+    .then(() => delayedColorChange('blue', 1000))
+    .then(() => delayedColorChange('indigo', 1000))
+    .then(() => delayedColorChange('violet', 1000))
