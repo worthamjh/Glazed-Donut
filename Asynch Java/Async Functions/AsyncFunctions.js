ff9471f411563eb240397db6333d50c1013ea724
worthamjh <worthamjh@gmail.com>
2026-02-09 10:08:38 -0600

    Comment out async color-change demo
    Commented out the delayedColorChange, rainbow, and printRainbow functions (the background color demo) to disable the demo without deleting the code. Added a separator and an "APIS and JSON" heading to prepare the file for new content. No other functional changes made.

--- Asynch Java/Async Functions/AsyncFunctions.js
+++ Asynch Java/Async Functions/AsyncFunctions.js
@@ -33,28 +33,30 @@
 // })
 
 
-const delayedColorChange = (color,delay) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            document.body.style.backgroundColor = color;
-            resolve();
-        }, delay)
-    })
-}
+// const delayedColorChange = (color,delay) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = color;
+//             resolve();
+//         }, delay)
+//     })
+// }
 
  
-async function rainbow() {
-    await delayedColorChange('red', 1000)
-    await delayedColorChange('orange', 1000)
-    await delayedColorChange('yellow', 1000)
-    await delayedColorChange('green', 1000)
-    await delayedColorChange('blue', 1000)
-    await delayedColorChange('indigo', 1000)
-    await delayedColorChange('violet', 1000)
-    return 'All done!'
-}
+// async function rainbow() {
+//     await delayedColorChange('red', 1000)
+//     await delayedColorChange('orange', 1000)
+//     await delayedColorChange('yellow', 1000)
+//     await delayedColorChange('green', 1000)
+//     await delayedColorChange('blue', 1000)
+//     await delayedColorChange('indigo', 1000)
+//     await delayedColorChange('violet', 1000)
+//     return 'All done!'
+// }
 
-async function printRainbow() {
-    await rainbow();
-    console.log('That is the end of the rainbow')
-}
+// async function printRainbow() {
+//     await rainbow();
+//     console.log('That is the end of the rainbow')
+// }
+// *****************************************************
+// APIS and JSON
