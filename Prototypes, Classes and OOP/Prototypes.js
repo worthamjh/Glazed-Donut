4c1f852ffe5cf3da167445b6ffd786800f629e25
worthamjh <worthamjh@gmail.com>
2026-02-12 14:41:56 -0600

    Add color factory and Color class examples
    Add helper functions (hex, rgb) and a makeColor factory that creates color objects with rgb() and hex() methods, plus a usage example (firstColor). Include a commented-out constructor/prototype implementation and an ES6 class Color with a greet() method and an instance (c1). Existing String/Array prototype overrides remain unchanged.

--- Prototypes, Classes and OOP/Prototypes.js
+++ Prototypes, Classes and OOP/Prototypes.js
@@ -5,3 +5,72 @@ String.prototype.yell = function(){
 Array.prototype.pop = function() {
     return 'I will never pop that element';
 };
+
+
+// ************************************************************************
+// FACTORY FUNCTION CREATING METHODS WITHIN AN OBJECT
+
+function hex(r,g,b) {
+    return '#' + ((1<<24) + (r<<16) + (g<<8) + b).toString(16).slice(1);
+}
+function rgb(r, g, b) {
+    return `rgb(${r}, ${g}, ${b})`;
+} 
+function makeColor(r,g,b){
+    const color= {};
+    color.r = r;
+    color.g = g;
+    color.b = b;
+    color.rgb = function() {
+        const {r, g, b} = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+    color.hex = function() {
+        const {r, g, b} = this;
+        return '#' + ((1<<24) + (r<<16) + (g<<8) + b).toString(16).slice(1);
+    }
+    return color;
+}
+const firstColor = makeColor(35,255,150);
+firstColor.rgb();
+firstColor.hex();
+
+
+
+// *****************************************************************************
+// CONSTRUCTOR FUNCTION
+// function Color(r,g,b){
+//     this.r = r;
+//     this.g = g;
+//     this.b = b;
+// }
+// Color.prototype.rgb = function(){
+//     const {r, g, b} = this;
+//     return `rgb(${r}, ${g}, ${b})`;
+// };
+// Color.prototype.hex = function() {
+//         const {r, g, b} = this;
+//         return '#' + ((1<<24) + (r<<16) + (g<<8) + b).toString(16).slice(1);
+// };
+// Color.prototype.rgba = function(a=1.0){
+//     const {r, g, b} = this;
+//     return `rgba(${r}, ${g}, ${b}, ${a})`;
+// };
+// const color1 =new Color(255,40,100);
+// const color2 =new Color(25,200,10);
+
+// ********************************************************************************************
+// JAVASCRIPT CLASSES
+class Color{
+    constructor(r,g,b,name){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+    greet(){
+        return `Hello from ${this.name}!`;
+    }
+}
+
+const c1 =  new Color(255, 67, 69, 'tomato');
